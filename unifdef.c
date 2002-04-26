ff144f16594350c180e7faf5d79094d86d8b5788
Tony Finch <dot@dotat.at>
2002-04-26 17:05:23 +0000

    more bool correctness

--- unifdef.c
+++ unifdef.c
@@ -44,7 +44,7 @@ static const char copyright[] =
 #ifdef __RCSID
 __RCSID("@(#)unifdef.c	8.1 (Berkeley) 6/6/93");
 __RCSID("$NetBSD: unifdef.c,v 1.8 2000/07/03 02:51:36 matt Exp $");
-__RCSID("$dotat: unifdef/unifdef.c,v 1.40 2002/04/26 17:02:17 fanf Exp $");
+__RCSID("$dotat: unifdef/unifdef.c,v 1.41 2002/04/26 17:05:23 fanf Exp $");
 #endif
 #ifdef __FBSDID
 __FBSDID("$FreeBSD$");
@@ -84,7 +84,7 @@ bool    complement;		/* -c option in effect: complement the
 #define MAXSYMS 1000
 char   *symname[MAXSYMS];	/* symbol name */
 char   *value[MAXSYMS];		/* -Dsym=value */
-char    ignore[MAXSYMS];	/* -iDsym or -iUsym */
+bool    ignore[MAXSYMS];	/* -iDsym or -iUsym */
 
 int     nsyms = 1;		/* symbol 0 is used for tracking #ifs */
 
