dfcfbabcda39fa720bb7cda32de89315621f1d68
Tony Finch <dot@dotat.at>
2002-04-29 03:07:20 +0000

    tidy up

--- unifdefall.sh
+++ unifdefall.sh
@@ -2,41 +2,25 @@
 #
 # remove all the #if's from a source file
 #
-# $dotat: unifdef/unifdefall.sh,v 1.3 2002/04/29 03:00:58 fanf Exp $
+# $dotat: unifdef/unifdefall.sh,v 1.4 2002/04/29 03:07:20 fanf Exp $
 
 set -e
 
 basename=`basename $0`
 tmp=`mktemp -d -t $basename` || exit 2
 
-# controlling symbols
 unifdef -s "$@" | sort | uniq > $tmp/ctrl
-
-# defined symbols and their values
-cpp -dM "$@" | sort | uniq |
-	sed -Ee 's/^#define[ 	]+//;s/[ 	]*$//' > $tmp/hashdefs
-
-# defined symbols without their values
+cpp -dM "$@" |
+	sed -Ee 's/^#define[ 	]+(.*[^	 ])[ 	]*$/\1/' > $tmp/hashdefs
 sed -Ee 's/^([A-Za-z0-9_]+).*$/\1/' $tmp/hashdefs > $tmp/alldef
-
-# all the -Ufoo arguments
 comm -23 $tmp/ctrl $tmp/alldef > $tmp/undef
-sed -Ee 's/^(.*)$/-U\1 \\/' $tmp/undef > $tmp/args.u
-
-# all the -Dfoo=bar arguments
 comm -12 $tmp/ctrl $tmp/alldef > $tmp/def
-while read sym
-do	sed -Ee '/^('"$sym"')([(][^)]*[)])?([ 	]+(.*))?$/!d;s//-D\1=\4/' $tmp/hashdefs
-done < $tmp/def |
-	sed -Ee 's/\\/\\\\/g;s/"/\\"/g;s/^/"/;s/$/" \\/' > $tmp/args.d
 
-# create the command line
 echo unifdef \\ > $tmp/cmd
-cat $tmp/args.d $tmp/args.u >> $tmp/cmd
+sed -Ee 's/^(.*)$/-U\1 \\/' $tmp/undef >> $tmp/cmd
+while read sym
+do	sed -Ee '/^('"$sym"')([(][^)]*[)])?([ 	]+(.*))$/!d;s//-D\1=\4/' $tmp/hashdefs
+done < $tmp/def |
+	sed -Ee 's/\\/\\\\/g;s/"/\\"/g;s/^/"/;s/$/" \\/' >> $tmp/cmd
 echo '"$@"' >> $tmp/cmd
-
-# do it all
 sh $tmp/cmd "$@"
-
-# clean up
-rm -r $tmp
