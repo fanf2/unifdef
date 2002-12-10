4fc9a85a10d799e240d42ec03e9df5f930560ae9
Tony Finch <dot@dotat.at>
2002-12-10 23:02:11 +0000

    Fix findsym()'s comment.

--- unifdef.c
+++ unifdef.c
@@ -44,7 +44,7 @@ static const char copyright[] =
 #ifdef __IDSTRING
 __IDSTRING(Berkeley, "@(#)unifdef.c	8.1 (Berkeley) 6/6/93");
 __IDSTRING(NetBSD, "$NetBSD: unifdef.c,v 1.8 2000/07/03 02:51:36 matt Exp $");
-__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.82 2002/12/10 23:01:04 fanf2 Exp $");
+__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.83 2002/12/10 23:02:11 fanf2 Exp $");
 #endif
 #ifdef __FBSDID
 __FBSDID("$FreeBSD: src/usr.bin/unifdef/unifdef.c,v 1.11 2002/09/24 19:27:44 fanf Exp $");
@@ -761,7 +761,7 @@ skipsym(const char *cp)
 
 /*
  * Look for the symbol in the symbol table. If is is found, we return
- * the symbol table index, else we return 0.
+ * the symbol table index, else we return -1.
  */
 int
 findsym(const char *str)
