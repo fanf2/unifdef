798d84bc625d3081dbf8179e12356b5cd89d1231
Tony Finch <dot@dotat.at>
2002-12-12 17:21:22 +0000

    Exit 2 on error.

--- unifdef.c
+++ unifdef.c
@@ -44,7 +44,7 @@ static const char copyright[] =
 #ifdef __IDSTRING
 __IDSTRING(Berkeley, "@(#)unifdef.c	8.1 (Berkeley) 6/6/93");
 __IDSTRING(NetBSD, "$NetBSD: unifdef.c,v 1.8 2000/07/03 02:51:36 matt Exp $");
-__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.102 2002/12/12 17:17:52 fanf2 Exp $");
+__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.103 2002/12/12 17:21:22 fanf2 Exp $");
 #endif
 #ifdef __FBSDID
 __FBSDID("$FreeBSD: src/usr.bin/unifdef/unifdef.c,v 1.11 2002/09/24 19:27:44 fanf Exp $");
@@ -861,8 +861,8 @@ void
 error(const char *msg)
 {
 	if (depth == 0)
-		errx(1, "%s: %d: %s", filename, linenum, msg);
+		errx(2, "%s: %d: %s", filename, linenum, msg);
 	else
-		errx(1, "%s: %d: %s (#if line %d depth %d)",
+		errx(2, "%s: %d: %s (#if line %d depth %d)",
 		    filename, linenum, msg, stifline[depth], depth);
 }
