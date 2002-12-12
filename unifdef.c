c2fff308cd9d2ba7531693734d7cad74111f74e2
Tony Finch <dot@dotat.at>
2002-12-12 17:03:37 +0000

    Move the error function back to the end

--- unifdef.c
+++ unifdef.c
@@ -44,7 +44,7 @@ static const char copyright[] =
 #ifdef __IDSTRING
 __IDSTRING(Berkeley, "@(#)unifdef.c	8.1 (Berkeley) 6/6/93");
 __IDSTRING(NetBSD, "$NetBSD: unifdef.c,v 1.8 2000/07/03 02:51:36 matt Exp $");
-__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.96 2002/12/12 17:02:08 fanf2 Exp $");
+__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.97 2002/12/12 17:03:37 fanf2 Exp $");
 #endif
 #ifdef __FBSDID
 __FBSDID("$FreeBSD: src/usr.bin/unifdef/unifdef.c,v 1.11 2002/09/24 19:27:44 fanf Exp $");
@@ -407,15 +407,6 @@ process(void)
  */
 
 /* utilities */
-void
-error(const char *msg)
-{
-	if (depth == 0)
-		errx(1, "%s: %d: %s", filename, linenum, msg);
-	else
-		errx(1, "%s: %d: %s (#if line %d depth %d)",
-		    filename, linenum, msg, stifline[depth], depth);
-}
 void nest(void) {
 	depth += 1;
 	if (depth >= MAXDEPTH)
@@ -863,4 +854,14 @@ debug(const char *msg, ...)
 		vwarnx(msg, ap);
 		va_end(ap);
 	}
+}
+
+void
+error(const char *msg)
+{
+	if (depth == 0)
+		errx(1, "%s: %d: %s", filename, linenum, msg);
+	else
+		errx(1, "%s: %d: %s (#if line %d depth %d)",
+		    filename, linenum, msg, stifline[depth], depth);
 }
