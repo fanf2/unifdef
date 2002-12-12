fbe2f9e308be8ad805b73f599e5ee665a8f98d62
Tony Finch <dot@dotat.at>
2002-12-12 19:45:29 +0000

    terminological consistency for pass states

--- unifdef.c
+++ unifdef.c
@@ -44,7 +44,7 @@ static const char copyright[] =
 #ifdef __IDSTRING
 __IDSTRING(Berkeley, "@(#)unifdef.c	8.1 (Berkeley) 6/6/93");
 __IDSTRING(NetBSD, "$NetBSD: unifdef.c,v 1.8 2000/07/03 02:51:36 matt Exp $");
-__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.115 2002/12/12 19:43:04 fanf2 Exp $");
+__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.116 2002/12/12 19:45:29 fanf2 Exp $");
 #endif
 #ifdef __FBSDID
 __FBSDID("$FreeBSD: src/usr.bin/unifdef/unifdef.c,v 1.11 2002/09/24 19:27:44 fanf Exp $");
@@ -126,7 +126,7 @@ typedef enum {
 	IS_PASS_MIDDLE,		/* first non-false #(el)if is unknown */
 	IS_FALSE_MIDDLE,	/* a false #elif after a pass state */
 	IS_TRUE_MIDDLE,		/* a true #elif after a pass state */
-	IS_PASS_ELSE,		/* an else after an unknown state */
+	IS_PASS_ELSE,		/* an else after a pass state */
 	IS_FALSE_ELSE,		/* an else after a true state */
 	IS_TRUE_ELSE,		/* an else after only false states */
 	IS_FALSE_TRAILER,	/* #elifs after a true are false */
