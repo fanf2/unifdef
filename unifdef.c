56876c8af1fdea8ba4647e82fca4861e75574637
Tony Finch <dot@dotat.at>
2003-01-20 12:03:48 +0000

    terminological consistency: s/modify/edit/

--- unifdef.c
+++ unifdef.c
@@ -44,7 +44,7 @@ static const char copyright[] =
 #ifdef __IDSTRING
 __IDSTRING(Berkeley, "@(#)unifdef.c	8.1 (Berkeley) 6/6/93");
 __IDSTRING(NetBSD, "$NetBSD: unifdef.c,v 1.8 2000/07/03 02:51:36 matt Exp $");
-__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.146 2003/01/20 12:03:10 fanf2 Exp $");
+__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.147 2003/01/20 12:03:48 fanf2 Exp $");
 #endif
 #ifdef __FBSDID
 __FBSDID("$FreeBSD: src/usr.bin/unifdef/unifdef.c,v 1.14 2003/01/17 19:12:02 fanf Exp $");
@@ -373,7 +373,7 @@ static void Oelif (void) { if (iocccok) Pelif(); else Eioccc(); }
 static void Idrop (void) { Fdrop();  ignoreon(); }
 static void Itrue (void) { Ftrue();  ignoreon(); }
 static void Ifalse(void) { Ffalse(); ignoreon(); }
-/* modify this line */
+/* edit this line */
 static void Mpass (void) { strncpy(keyword, "if  ", 4); Pelif(); }
 static void Mtrue (void) { keywordedit("else\n");  state(IS_TRUE_MIDDLE); }
 static void Melif (void) { keywordedit("endif\n"); state(IS_FALSE_TRAILER); }
