83ddac7487f3a1c8eee90ca087a211dd38c3b3d8
Tony Finch <dot@dotat.at>
2003-01-20 00:59:36 +0000

    Be more explicit about the failure mode.
    Reported by: Poul-Henning Kamp <phk@FreeBSD.org>

--- unifdef.c
+++ unifdef.c
@@ -44,7 +44,7 @@ static const char copyright[] =
 #ifdef __IDSTRING
 __IDSTRING(Berkeley, "@(#)unifdef.c	8.1 (Berkeley) 6/6/93");
 __IDSTRING(NetBSD, "$NetBSD: unifdef.c,v 1.8 2000/07/03 02:51:36 matt Exp $");
-__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.135 2003/01/20 00:50:06 fanf2 Exp $");
+__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.136 2003/01/20 00:59:36 fanf2 Exp $");
 #endif
 #ifdef __FBSDID
 __FBSDID("$FreeBSD: src/usr.bin/unifdef/unifdef.c,v 1.11 2002/09/24 19:27:44 fanf Exp $");
@@ -881,8 +881,9 @@ static void
 error(const char *msg)
 {
 	if (depth == 0)
-		errx(2, "%s: %d: %s", filename, linenum, msg);
+		warnx("%s: %d: %s", filename, linenum, msg);
 	else
-		errx(2, "%s: %d: %s (#if line %d depth %d)",
+		warnx("%s: %d: %s (#if line %d depth %d)",
 		    filename, linenum, msg, stifline[depth], depth);
+	errx(2, "output may be truncated");
 }
