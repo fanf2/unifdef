e2eea80a7be42b6fa4255bc1d31d45cfc17ae093
Tony Finch <dot@dotat.at>
2003-01-20 11:46:53 +0000

    more idiomatic strlcpy usage

--- unifdef.c
+++ unifdef.c
@@ -44,7 +44,7 @@ static const char copyright[] =
 #ifdef __IDSTRING
 __IDSTRING(Berkeley, "@(#)unifdef.c	8.1 (Berkeley) 6/6/93");
 __IDSTRING(NetBSD, "$NetBSD: unifdef.c,v 1.8 2000/07/03 02:51:36 matt Exp $");
-__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.144 2003/01/20 11:45:32 fanf2 Exp $");
+__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.145 2003/01/20 11:46:53 fanf2 Exp $");
 #endif
 #ifdef __FBSDID
 __FBSDID("$FreeBSD: src/usr.bin/unifdef/unifdef.c,v 1.14 2003/01/17 19:12:02 fanf Exp $");
@@ -445,7 +445,7 @@ ignoreon(void)
 static void
 keywordedit(const char *replacement)
 {
-	strlcpy(keyword, replacement, EDITSLOP);
+	strlcpy(keyword, replacement, tline + sizeof(tline) - keyword);
 }
 static void
 nest(void)
