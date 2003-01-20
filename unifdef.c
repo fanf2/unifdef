f17a5f02310e39b194f6e1286848a1695fb8ee9a
Tony Finch <dot@dotat.at>
2003-01-20 00:50:06 +0000

    typo: maintin -> maintain

--- unifdef.c
+++ unifdef.c
@@ -44,7 +44,7 @@ static const char copyright[] =
 #ifdef __IDSTRING
 __IDSTRING(Berkeley, "@(#)unifdef.c	8.1 (Berkeley) 6/6/93");
 __IDSTRING(NetBSD, "$NetBSD: unifdef.c,v 1.8 2000/07/03 02:51:36 matt Exp $");
-__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.134 2003/01/20 00:01:49 fanf2 Exp $");
+__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.135 2003/01/20 00:50:06 fanf2 Exp $");
 #endif
 #ifdef __FBSDID
 __FBSDID("$FreeBSD: src/usr.bin/unifdef/unifdef.c,v 1.11 2002/09/24 19:27:44 fanf Exp $");
@@ -298,7 +298,7 @@ usage(void)
  * indicate that processing is complete.
  *
  * Nesting is handled by keeping a stack of states; some transition
- * functions increase or decrease the depth. They also maintin the
+ * functions increase or decrease the depth. They also maintain the
  * ignore state on a stack. In some complicated cases they have to
  * alter the preprocessor directive, as follows.
  *
