020367b4d8a6a840b4a44d6e8568d681d7038a49
Tony Finch <dot@dotat.at>
2003-07-31 07:59:15 +0000

    Remove clause three of the licence, as permitted by Berkeley. OpenBSD have done this globally, but Net- and Free- haven't. I've had two questions about this recently, and since I don't distribute Berkeley's licence change statement this is a good way to be clear.

--- unifdef.c
+++ unifdef.c
@@ -14,11 +14,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -46,7 +42,7 @@ static const char copyright[] =
 #ifdef __IDSTRING
 __IDSTRING(Berkeley, "@(#)unifdef.c	8.1 (Berkeley) 6/6/93");
 __IDSTRING(NetBSD, "$NetBSD: unifdef.c,v 1.8 2000/07/03 02:51:36 matt Exp $");
-__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.161 2003/07/01 15:32:48 fanf2 Exp $");
+__IDSTRING(dotat, "$dotat: unifdef/unifdef.c,v 1.162 2003/07/31 07:59:15 fanf2 Exp $");
 #endif
 #endif /* not lint */
 #ifdef __FBSDID
