ce59e840d8deb9329628abde0b3dd6bf0d6d6a1d
Tony Finch <dot@dotat.at>
2009-11-25 18:00:54 +0000

    unifdefall: allow spaces in $TMPDIR
    Submitted-by: Jonathan Nieder <jrnieder@gmail.com>

--- unifdefall.sh
+++ unifdefall.sh
@@ -25,27 +25,27 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-#	$dotat: unifdef/unifdefall.sh,v 1.14 2009/11/25 17:56:10 fanf2 Exp $
+#	$dotat: unifdef/unifdefall.sh,v 1.15 2009/11/25 18:00:54 fanf2 Exp $
 
 set -e
 
 basename=$(basename $0)
 tmp=$(mktemp -d -t "${TMPDIR:-/tmp}/$basename.XXXXXXXXXX") || exit 2
 
-unifdef -s "$@" | sort | uniq > $tmp/ctrl
+unifdef -s "$@" | sort | uniq >"$tmp/ctrl"
 cpp -dM "$@" | sort |
-	sed -Ee 's/^#define[ 	]+(.*[^	 ])[ 	]*$/\1/' > $tmp/hashdefs
-sed -Ee 's/^([A-Za-z0-9_]+).*$/\1/' $tmp/hashdefs > $tmp/alldef
-comm -23 $tmp/ctrl $tmp/alldef > $tmp/undef
-comm -12 $tmp/ctrl $tmp/alldef > $tmp/def
+	sed -Ee 's/^#define[ 	]+(.*[^	 ])[ 	]*$/\1/' >"$tmp/hashdefs"
+sed -Ee 's/^([A-Za-z0-9_]+).*$/\1/' "$tmp/hashdefs" >"$tmp/alldef"
+comm -23 "$tmp/ctrl" "$tmp/alldef" >"$tmp/undef"
+comm -12 "$tmp/ctrl" "$tmp/alldef" >"$tmp/def"
 
-echo unifdef -k \\ > $tmp/cmd
-sed -Ee 's/^(.*)$/-U\1 \\/' $tmp/undef >> $tmp/cmd
+echo unifdef -k \\ >"$tmp/cmd"
+sed -Ee 's/^(.*)$/-U\1 \\/' "$tmp/undef" >>"$tmp/cmd"
 while read sym
-do	sed -Ee '/^('"$sym"')([(][^)]*[)])?([ 	]+(.*))?$/!d;s//-D\1=\4/' $tmp/hashdefs
-done < $tmp/def |
-	sed -Ee 's/\\/\\\\/g;s/"/\\"/g;s/^/"/;s/$/" \\/' >> $tmp/cmd
-echo '"$@"' >> $tmp/cmd
-sh $tmp/cmd "$@"
+do	sed -Ee '/^('"$sym"')([(][^)]*[)])?([ 	]+(.*))?$/!d;s//-D\1=\4/' "$tmp/hashdefs"
+done <"$tmp/def" |
+	sed -Ee 's/\\/\\\\/g;s/"/\\"/g;s/^/"/;s/$/" \\/' >>"$tmp/cmd"
+echo '"$@"' >>"$tmp/cmd"
+sh "$tmp/cmd" "$@"
 
-rm -r $tmp
+rm -r "$tmp"
