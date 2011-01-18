58e40f5d5e35d5b1d855a5bac67caf7ec40635ee
Tony Finch <dot@dotat.at>
2011-01-18 19:51:40 +0000

    Update copyright date.

--- unifdef.c
+++ unifdef.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002 - 2010 Tony Finch <dot@dotat.at>
+ * Copyright (c) 2002 - 2011 Tony Finch <dot@dotat.at>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
