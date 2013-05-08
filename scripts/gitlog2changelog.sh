309ae852d3be97c179dc82be9bfc0bd38799feb9
Tony Finch <dot@dotat.at>
2013-05-08 16:08:49 +0100

    scripts: fold long lines in the Changelog

--- scripts/gitlog2changelog.sh
+++ scripts/gitlog2changelog.sh
@@ -3,6 +3,6 @@
 line="---------------------------------------------------"
 
 git log --no-merges --decorate -M --stat --pretty=format:"$line%n%ai %an <%ae>%d%n%n%s%n%n%b" |
-uniq
+uniq | fold -s
 echo
 echo $line
