5d452063f1d6f75ae1d1725f037f4546ed8f8306
Tony Finch <dot@dotat.at>
2013-05-08 16:30:35 +0100

    tests: do not ship vast chunks of XTerm
    By default the xterm test does nothing; if required it will download
    and upack xterm in order to run the full test. It is likely to break
    when a new version of xterm is released since the download URL is
    not versioned.

--- tests/xterm.sh
+++ tests/xterm.sh
@@ -1,8 +1,36 @@
-unifdef -s xterm-292-main.c | sed 's/^/#undef /' >xterm-undefs.h
+# hacks so we don't have to distribute huge chunks of XTerm
+
+if [ ! -f xterm.really ]
+then
+	cat xterm.expout
+	cat xterm.experr 1>&2
+	exit $(cat xterm.exprc)
+fi
+if [ -f xterm.clean ]
+then
+	rm xterm.tar.gz xterm-defs.h xterm-main.c
+fi
+
+if [ ! -f xterm.tar.gz ]
+then
+	wget -q http://invisible-island.net/datafiles/release/xterm.tar.gz
+fi
+if [ ! -f xterm-main.c ]
+then
+	tar xf xterm.tar.gz
+	cd xterm-[0-9][0-9][0-9]
+	gcc -I/usr/X11R6/include -I. -E -dM \
+		main.c > ../xterm-defs.h
+	cat     main.c > ../xterm-main.c
+	cd ..
+	rm -r xterm-[0-9][0-9][0-9]
+fi
+
+unifdef -s xterm-main.c | sed 's/^/#undef /' >xterm-undefs.h
 echo $? 1>&2
-unifdef -f xterm-undefs.h -f xterm-defs.h xterm-292-main.c >xterm-main.c
+unifdef -f xterm-undefs.h -f xterm-defs.h xterm-main.c >xterm-out.c
 echo $? 1>&2
-cat xterm-main.c
-grep '#if' xterm-main.c
+cat xterm-out.c
+grep '#if' xterm-out.c
 echo $? 1>&2
-rm -f xterm-undefs.h xterm-main.c
+rm -f xterm-undefs.h xterm-out.c
