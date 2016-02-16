7f14db7f166c422ba29fe1023a67b2595cb9a7a4
Tony Finch <dot@dotat.at>
2016-02-16 14:06:53 +0000

    win32: revert mktempmode() change - it does not help

--- win32/win32.c
+++ win32/win32.c
@@ -41,10 +41,8 @@ replace(const char *old, const char *new)
 FILE *
 mktempmode(char *tmp, int mode)
 {
-	char *name = _mktemp(tmp);
-	FILE *fp = fopen(name, "wb");
-	_chmod(name, mode);
-	return (fp);
+	mode = mode;
+	return (fopen(_mktemp(tmp), "wb"));
 }
 
 FILE *
