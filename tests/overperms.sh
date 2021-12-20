65ae6ddb3720310eb7763abfcfc5a6a95353abd9
Tomi Ollila <tomi.ollila@iki.fi>
2021-12-20 20:11:33 +0200

    tests: support both *BSD and Linux ls -l permissions output
    Linux ls(1) adds trailing '.' to the first -- "drwxrwxrwx" output
    column -- a character that does not exist in *BSD ls -l output.
    
    The sed expression 's/[.]\? .*//' removes everything since first
    space -- and additionally trailing dot (.) before that if such
    a character exists there.

--- tests/overperms.sh
+++ tests/overperms.sh
@@ -1,11 +1,11 @@
 cp if1.c outfile.c
 chmod 640 outfile.c
-ls -l outfile.c | cut -d' ' -f1 1>&2
+ls -l outfile.c | sed 's/[.]\? .*//' 1>&2
 unifdef -DFOO=1 -DFOOB=42 -UBAR -m outfile.c
 e=$?
 case ${BUILD_MINGW} in
 (yes)	printf '%s\n' '-rw-r-----' 1>&2 ;;
-(*)	ls -l outfile.c | cut -d' ' -f1 1>&2 ;;
+(*)	ls -l outfile.c | sed 's/[.]\? .*//' 1>&2 ;;
 esac
 cat outfile.c
 rm outfile.c
